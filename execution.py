5dacbc335a6afeda869fb28f663b4c10bb2a523d
levelslh1213 <pauloleonardopaes@gmail.com>
2024-01-23 15:50:12 -0300

    Feat: Implementings execution log routine

--- execution.py
+++ execution.py
@@ -2,29 +2,54 @@ import pyodbc as db
 from connections import getConnection
 import io
 
-def getSQL(fileName: str):
-    sql = ''
-    with open(f'SQLs\{fileName}.sql', 'r', encoding='UTF-8')as f:
-        sql += f.read()
-    return sql
+class Execution: 
+
+    def __init__(self, connectionString: None, sqlFileName: None) -> None:
+        self.connectionString = connectionString
+        self.sqlFileName = sqlFileName
+        self.connection = db.connect(self.connectionString)
+
+    def getSQL(self):
+        sql = ''
+        with open(f'SQLs\{self.sqlFileName}.sql', 'r', encoding='UTF-8')as f:
+            sql += f.read()
+        return sql
     
+    def insertExecutionLog(self, rowCount= 0, message= ""):
+        
+        try:
+            exec = self.connection.cursor()
+            script = F"INSERT INTO PRO021LOG (DATA_EXEC, GN_MESSAGE, NR_LINHAS) VALUES (GETDATE(), \'{str(message)}\', {str(rowCount)})"
+            exec.execute(script)
+            exec.commit()
+        except db.Error as e:
+            print(f"Erro ao inserir logs de execução!\n {e}")
 
-def execSQL():
-    connectionString, sqlFileName = getConnection()
-    sql = getSQL(sqlFileName)
 
-    try:
-        connection = db.connect(connectionString)
-        exec = connection.cursor()
-        response = exec.execute(sql)
-        # print(response.rowcount)
-        response.commit()
+    def execSQL(self):
+        # connectionString, sqlFileName = getConnection()
+        sql = self.getSQL()
 
-        # lines = exec.fetchall()
+        try:
+            exec = self.connection.cursor()
+            response = exec.execute(sql)
+            response.commit()
 
-        # for line in lines: 
-        #     print(str(line))
-    except (db.Error) as e:
-        print(f"Erro ao executar script na base de dados: \n {e}")
-        return "Error"
-    
+            # lines = exec.fetchall()
+
+            # for line in lines: 
+            #     print(str(line))
+        except (db.Error) as e:
+            print(f"Erro ao executar script na base de dados: \n {e}")
+            return "Error"
+        
+        return response.rowcount
+        
+    def endConnection(self):
+        try:
+            self.connection.close()
+        except db.Error as e:
+            print(f"Error while closing connection: {e}")
+            return "Error"
+        return "Connection Closed!"
+        
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-from execution import execSQL
+from execution import Execution
+from connections import getConnection
 
-execSQL()
+connectionString, sqlFileName = getConnection()
+execution = Execution(connectionString=connectionString, sqlFileName=sqlFileName)
+rowcount = execution.execSQL()
+execution.insertExecutionLog(rowCount=rowcount, message="Sucesso!")
+execution.endConnection()
